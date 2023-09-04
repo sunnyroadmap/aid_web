f81fd3253ff646bb8ccf2dd3fa825228da569037
sunnyroadmap <makeeloveenotwarr@gmail.com>
2023-09-04 17:54:08 +0100

    Check

--- js/translate.js
+++ js/translate.js
@@ -1,52 +1,38 @@
-// Define the translations variable at a higher scope
-let translations = {};
-
-// Define the languageLinks variable to store language option elements
-const languageLinks = document.querySelectorAll('.dropdown-content a');
-
-// Function to load translations from a JSON file
 function loadTranslations(callback) {
-  fetch('locale/translations.json') // Replace with the correct path to your translations file
-    .then((response) => response.json())
-    .then((data) => {
-      translations = data; // Store the loaded translations in the global variable
-      callback();
-    })
-    .catch((error) => {
-      console.error('Translation file load error:', error);
-    });
-}
-
-// Function to translate content based on the selected language
-function translateContent(selectedLang) {
-  console.log('Translating to:', selectedLang); // Debug
-  const elementsToTranslate = document.querySelectorAll('[data-translate]');
+    fetch('locale/translations.json')
+      .then((response) => response.json())
+      .then((data) => {
+        translations = data;
+        callback();
+      })
+      .catch((error) => {
+        console.error('Translation file load error:', error);
+      });
+  }
   
-  elementsToTranslate.forEach((element) => {
-    const translationKey = element.getAttribute('data-translate');
+  function translateContent(selectedLang) {
+    console.log('Translating to:', selectedLang); // Debug
+    const elementsToTranslate = document.querySelectorAll('[data-translate]');
     
-    // Check if translations[selectedLang] and translations[selectedLang][translationKey] are defined
-    if (translations[selectedLang] && translations[selectedLang][translationKey]) {
-      element.textContent = translations[selectedLang][translationKey];
-    }
-  });
-}
-
-// Add click event listeners to language options in the dropdown
-languageLinks.forEach((link) => {
-  link.addEventListener('click', (event) => {
-    event.preventDefault();
-    
-    // Ensure that the clicked element has the data-lang attribute
-    if (event.target.getAttribute('data-lang')) {
-      const selectedLang = event.target.getAttribute('data-lang');
+    elementsToTranslate.forEach((element) => {
+      const translationKey = element.getAttribute('data-translate');
+      const translatedText = translations[selectedLang][translationKey];
+  
+      if (translatedText) {
+        element.textContent = translatedText;
+      }
+    });
+  }
+  
+  // ...
+  
+  // Add click event listeners to language options in the dropdown
+  languageOptions.forEach((option) => {
+    option.addEventListener('click', (event) => {
+      event.preventDefault();
+      const selectedLang = option.getAttribute('data-lang');
+      languageSwitcher.setAttribute('data-selected-lang', selectedLang);
       translateContent(selectedLang);
-    }
+    });
   });
-});
-
-// Initial translation based on the selected language
-const initialSelectedLang = languageSwitcher.getAttribute('data-selected-lang');
-loadTranslations(() => {
-  translateContent(initialSelectedLang);
-});
+  
