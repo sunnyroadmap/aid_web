aa1b836d6835102a59885495a6d069eabd34cdb0
sunnyroadmap <makeeloveenotwarr@gmail.com>
2023-09-04 17:56:52 +0100

    Old version try

--- js/translate.js
+++ js/translate.js
@@ -1,38 +1,54 @@
+
+// Define the translations variable at a higher scope
+let translations = {};
+
+// Define the languageOptions variable to store language option elements
+const languageOptions = document.querySelectorAll('.dropdown-content a');
+
+// Function to load translations from a JSON file
 function loadTranslations(callback) {
-    fetch('locale/translations.json')
-      .then((response) => response.json())
-      .then((data) => {
-        translations = data;
-        callback();
-      })
-      .catch((error) => {
-        console.error('Translation file load error:', error);
-      });
-  }
-  
-  function translateContent(selectedLang) {
-    console.log('Translating to:', selectedLang); // Debug
-    const elementsToTranslate = document.querySelectorAll('[data-translate]');
-    
-    elementsToTranslate.forEach((element) => {
-      const translationKey = element.getAttribute('data-translate');
-      const translatedText = translations[selectedLang][translationKey];
-  
-      if (translatedText) {
-        element.textContent = translatedText;
-      }
+  fetch('locale/translations.json') // Replace with the correct path to your translations file
+    .then((response) => response.json())
+    .then((data) => {
+      translations = data; // Store the loaded translations in the global variable
+      callback();
+    })
+    .catch((error) => {
+      console.error('Translation file load error:', error);
     });
-  }
-  
-  // ...
-  
-  // Add click event listeners to language options in the dropdown
-  languageOptions.forEach((option) => {
+}
+
+// Function to translate content based on the selected language
+function translateContent(selectedLang) {
+  const elementsToTranslate = document.querySelectorAll('[data-translate]');
+
+  elementsToTranslate.forEach((element) => {
+    const translationKey = element.getAttribute('data-translate');
+    const translatedText = translations[selectedLang][translationKey];
+
+    if (translatedText) {
+      element.textContent = translatedText;
+    }
+  });
+}
+
+// Add click event listeners to language options in the dropdown
+languageOptions.forEach((option) => {
     option.addEventListener('click', (event) => {
       event.preventDefault();
       const selectedLang = option.getAttribute('data-lang');
+  
+      // Update the selected language and translate content
       languageSwitcher.setAttribute('data-selected-lang', selectedLang);
       translateContent(selectedLang);
     });
   });
+  
+// Initial translation based on the selected language
+const initialSelectedLang = languageSwitcher.getAttribute('data-selected-lang');
+loadTranslations(() => {
+  translateContent(initialSelectedLang);
+});
+
+
   
