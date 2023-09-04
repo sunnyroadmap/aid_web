8fe247dbe9a5449be26e347a787f7e2f77975dc6
sunnyroadmap <makeeloveenotwarr@gmail.com>
2023-09-04 15:48:10 +0100

    Translation system

---
+++ js/translate.js
@@ -0,0 +1,46 @@
+// Function to load translations from a JSON file
+function loadTranslations(callback) {
+    fetch('locale/translations.json') // Replace with the path to your translations file
+      .then((response) => response.json())
+      .then((data) => {
+        callback(data);
+      })
+      .catch((error) => {
+        console.error('Translation file load error:', error);
+      });
+  }
+  
+  // Function to translate content based on the selected language
+  function translateContent(translations, selectedLang) {
+    const elementsToTranslate = document.querySelectorAll('[data-translate]');
+  
+    elementsToTranslate.forEach((element) => {
+      const translationKey = element.getAttribute('data-translate');
+      const translatedText = translations[selectedLang][translationKey];
+  
+      if (translatedText) {
+        element.textContent = translatedText;
+      }
+    });
+  }
+  
+  // Rest of the code remains the same
+  
+  // Add click event listeners to language options in the dropdown
+  languageOptions.forEach((option) => {
+    option.addEventListener('click', (event) => {
+      event.preventDefault();
+      const selectedLang = option.getAttribute('data-lang');
+  
+      // Update the selected language and translate content
+      languageSwitcher.setAttribute('data-selected-lang', selectedLang);
+      translateContent(translations, selectedLang);
+    });
+  });
+  
+  // Initial translation based on the selected language
+  const initialSelectedLang = languageSwitcher.getAttribute('data-selected-lang');
+  loadTranslations((translations) => {
+    translateContent(translations, initialSelectedLang);
+  });
+  
