c40397a9b5c868450a4a50130c37d643156e150a
sunnyroadmap <makeeloveenotwarr@gmail.com>
2023-09-04 15:28:19 +0100

    Pre-final fix. Moving bottom 5-10px left to fix

--- js/langswitch.js
+++ js/langswitch.js
@@ -1,6 +1,7 @@
 // Get references to the language switcher and dropdown content
 const languageSwitcher = document.querySelector('.language-switcher');
 const languageDropdown = document.querySelector('.language-switcher + .dropdown-content');
+const languageSwitcherContainer = document.querySelector('.language-switcher-container');
 
 // Function to toggle the visibility of the dropdown content
 function toggleDropdown(dropdown) {
@@ -11,37 +12,22 @@ function toggleDropdown(dropdown) {
   }
 }
 
-// Function to handle language selection
-function handleLanguageSelection(event) {
-  event.preventDefault();
-  event.stopPropagation();
+// Function to position the language switcher container
+function positionLanguageSwitcher() {
+  // Calculate the new top position based on the viewport's scroll position
+  const scrollTop = window.scrollY || document.documentElement.scrollTop;
+  const newTopPosition = 80; // Adjust this value as needed
 
-  // Check if a language data attribute is present
-  const selectedLang = event.target.getAttribute('data-lang');
-
-  if (selectedLang) {
-    // Change the selected flag to the chosen language
-    const selectedFlag = languageSwitcher.querySelector('.flag-selected');
-    selectedFlag.src = `img/${selectedLang}.png`;
-
-    // Close the dropdown
-    toggleDropdown(languageDropdown);
-  }
+  // Set the new top position for the language switcher container
+  languageSwitcherContainer.style.top = `${newTopPosition}px`;
 }
 
 // Add a click event listener to the language switcher
 languageSwitcher.addEventListener('click', (event) => {
-  handleLanguageSelection(event);
+  event.preventDefault();
+  event.stopPropagation();
   // Toggle the visibility of the language dropdown
   toggleDropdown(languageDropdown);
-});
-
-// Add click event listeners to language options in the dropdown
-const languageOptions = languageDropdown.querySelectorAll('a');
-languageOptions.forEach((option) => {
-  option.addEventListener('click', (event) => {
-    handleLanguageSelection(event);
-  });
 });
 
 // Add a click event listener to the document to close the dropdown when clicking outside
@@ -50,3 +36,26 @@ document.addEventListener('click', (event) => {
     languageDropdown.style.display = 'none';
   }
 });
+
+// Add click event listeners to language options
+const languageOptions = document.querySelectorAll('.dropdown-content a');
+const flagIcon = document.querySelector('.language-flag');
+
+languageOptions.forEach((option) => {
+  option.addEventListener('click', (event) => {
+    event.preventDefault();
+    const selectedLang = option.getAttribute('data-lang');
+    const flagImg = option.querySelector('.flag-icon');
+
+    // Update the flag icon with the selected language's flag
+    flagIcon.src = flagImg.src;
+
+    // You can also handle language-specific actions here
+
+    // Hide the dropdown
+    languageDropdown.style.display = 'none';
+  });
+});
+
+// Initial positioning of the language switcher container
+positionLanguageSwitcher();
