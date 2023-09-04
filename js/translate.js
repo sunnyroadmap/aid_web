763f51da41c476d507003feb19036ff7107eeeda
sunnyroadmap <makeeloveenotwarr@gmail.com>
2023-09-04 17:17:06 +0100

    Test 4

--- js/translate.js
+++ js/translate.js
@@ -1,9 +1,6 @@
 // Define the translations variable at a higher scope
 let translations = {};
 
-// Define the language switcher element
-const languageSwitcher = document.querySelector('.language-switcher');
-
 // Function to load translations from a JSON file
 function loadTranslations(callback) {
   fetch('locale/translations.json') // Replace with the correct path to your translations file
@@ -19,29 +16,30 @@ function loadTranslations(callback) {
 
 // Function to translate content based on the selected language
 function translateContent(selectedLang) {
-  console.log('Translating to:', selectedLang); // Debug
   const elementsToTranslate = document.querySelectorAll('[data-translate]');
-  
+
   elementsToTranslate.forEach((element) => {
     const translationKey = element.getAttribute('data-translate');
-    
-    // Check if translations[selectedLang] and translations[selectedLang][translationKey] are defined
-    if (translations[selectedLang] && translations[selectedLang][translationKey]) {
-      element.textContent = translations[selectedLang][translationKey];
+    const translatedText = translations[selectedLang][translationKey];
+
+    if (translatedText) {
+      element.textContent = translatedText;
     }
   });
 }
 
-// Add click event listener to the language switcher
-languageSwitcher.addEventListener('click', (event) => {
-  event.preventDefault();
-  
-  // Ensure that the clicked element has the data-lang attribute
-  if (event.target.getAttribute('data-lang')) {
-    const selectedLang = event.target.getAttribute('data-lang');
+// Rest of the code remains the same
+
+// Add click event listeners to language options in the dropdown
+languageOptions.forEach((option) => {
+  option.addEventListener('click', (event) => {
+    event.preventDefault();
+    const selectedLang = option.getAttribute('data-lang');
+
+    // Update the selected language and translate content
     languageSwitcher.setAttribute('data-selected-lang', selectedLang);
     translateContent(selectedLang);
-  }
+  });
 });
 
 // Initial translation based on the selected language
