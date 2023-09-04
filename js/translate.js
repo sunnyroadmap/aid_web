5e61c2bb47925c137f8201c7531969398a011b5c
sunnyroadmap <makeeloveenotwarr@gmail.com>
2023-09-04 16:11:00 +0100

    Code workaround

--- js/translate.js
+++ js/translate.js
@@ -7,55 +7,43 @@ function loadTranslations(callback) {
     .then((response) => response.json())
     .then((data) => {
       translations = data; // Store the loaded translations in the global variable
-      callback(data);
+      callback();
     })
     .catch((error) => {
       console.error('Translation file load error:', error);
     });
 }
-  
-  // Function to translate content based on the selected language
-  function translateContent(translations, selectedLang) {
-    const elementsToTranslate = document.querySelectorAll('[data-translate]');
-  
-    elementsToTranslate.forEach((element) => {
-      const translationKey = element.getAttribute('data-translate');
-      const translatedText = translations[selectedLang][translationKey];
-  
-      if (translatedText) {
-        element.textContent = translatedText;
-      }
-    });
-  }
-  
-  // Rest of the code remains the same
-  
+
+// Function to translate content based on the selected language
+function translateContent(selectedLang) {
+  const elementsToTranslate = document.querySelectorAll('[data-translate]');
+
+  elementsToTranslate.forEach((element) => {
+    const translationKey = element.getAttribute('data-translate');
+    const translatedText = translations[selectedLang][translationKey];
+
+    if (translatedText) {
+      element.textContent = translatedText;
+    }
+  });
+}
+
+// Rest of the code remains the same
+
 // Add click event listeners to language options in the dropdown
 languageOptions.forEach((option) => {
-    option.addEventListener('click', (event) => {
-      event.preventDefault();
-      const selectedLang = option.getAttribute('data-lang');
-  
-      // Update the selected language and translate content
-      languageSwitcher.setAttribute('data-selected-lang', selectedLang);
-      translateContent(translations, selectedLang);
-    });
+  option.addEventListener('click', (event) => {
+    event.preventDefault();
+    const selectedLang = option.getAttribute('data-lang');
+
+    // Update the selected language and translate content
+    languageSwitcher.setAttribute('data-selected-lang', selectedLang);
+    translateContent(selectedLang);
   });
-  
-  // Initial translation based on the selected language
-  const initialSelectedLang = languageSwitcher.getAttribute('data-selected-lang');
-  loadTranslations((translations) => {
-    translateContent(translations, initialSelectedLang);
-  });
-  
-  // Add click event listeners to language options in the dropdown
-languageOptions.forEach((option) => {
-    option.addEventListener('click', (event) => {
-      event.preventDefault();
-      const selectedLang = option.getAttribute('data-lang');
-  
-      // Update the selected language and translate content
-      languageSwitcher.setAttribute('data-selected-lang', selectedLang);
-      translateContent(translations, selectedLang);
-    });
-  }); 
+});
+
+// Initial translation based on the selected language
+const initialSelectedLang = languageSwitcher.getAttribute('data-selected-lang');
+loadTranslations(() => {
+  translateContent(initialSelectedLang);
+});
